93e1b5f7a557b7ad73bab6ef2a5b83c31f5acaa4
codeitfast <micahkurlandcohen@gmail.com>
2024-12-03 14:48:21 -0500

    new redirect

--- redirector/background.js
+++ redirector/background.js
@@ -6,6 +6,7 @@ const destinationSite = "https://www.duolingo.com/learn";
 
 // Listen for web navigation
 chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+  if(details.frameId != 0) return // this way embedded youtube videos don't lead to redirects, etc
   if (sitesToRedirect.some((site) => details.url.includes(site))) {
     chrome.tabs.update(details.tabId, {
       url: destinationSite,
